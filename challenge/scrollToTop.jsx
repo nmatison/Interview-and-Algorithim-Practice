b487cebe4ccb4e57344e4b482519ee8c8e313d1f
Nick <nmatison@gmail.com>
2018-09-29 11:57:29 -0700

    Add a function for the react challenge project

--- challenge/scrollToTop.jsx
+++ challenge/scrollToTop.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { withRouter } from 'react-router';
+import React from "react";
+import { withRouter } from "react-router";
+import { Route } from 'react-router-dom';
+
 
 class ScrollToTop extends React.Component {
+  componentDidUpdate() {
+    window.scrollTo(0, 0);
+  }
 
+  renderComponent() {
+    console.log(this.props.location);
+    return <Route exact path={`${this.props.location.pathname}`} component={} />;
+  }
 
   render() {
+    return <div>{this.renderComponent()}</div>;
   }
 }
 
