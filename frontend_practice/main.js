37eeb5b8927c24e8e54b950901b4fc26275dc322
Nick <nmatison@gmail.com>
2018-09-29 09:44:05 -0700

    add functionality to change div color to buttons

--- frontend_practice/main.js
+++ frontend_practice/main.js
@@ -1,6 +1,12 @@
 document.addEventListener("load",() => {
   let target = document.getElementById("target");
-  target.innerHTML += `<input type="submit" onClick=${changeColor("blue")} value="Blue" />`
-  target.innerHTML += `<input type="submit" onClick=${changeColor("green")} value="Green" />`
-  target.innerHTML += `<input type="submit" onClick=${changeColor("red")} value="Red" />`
-})
+  target.innerHTML += `<input type="submit" onClick=${changeColor("blue")} value="Blue" />`;
+  target.innerHTML += `<input type="submit" onClick=${changeColor("green")} value="Green" />`;
+  target.innerHTML += `<input type="submit" onClick=${changeColor("red")} value="Red" />`;
+});
+
+const changeColor = function(color) {
+  let target = document.getElementById("target");
+  target.setAttribute("style", "background-color", `${color}`)
+
+};
