806512dc9fa49671b1906dc9b3a754fc03bfc642
Nick <nmatison@gmail.com>
2018-09-29 13:13:33 -0700

    Cut out everything in scrollToTop.jsx as react can read props.children

--- challenge/scrollToTop.jsx
+++ challenge/scrollToTop.jsx
@@ -8,24 +8,8 @@ class ScrollToTop extends React.Component {
     window.scrollTo(0, 0);
   }
 
-  renderComponent() {
-    console.log(this.props.children);
-    let childComponent = this.findChild(this.props.location.pathname)
-    // console.log(childComponent);
-    return <Route exact path={`${this.props.location.pathname}`} component={childComponent} />;
-  }
-
-  findChild(location) {
-    let children = this.props.children;
-    for (let i = 0; i < children.length; i++) {
-      let childLocation = children[i].props.path;
-      let childComponent = children[i].props.component;
-      if (location == childLocation) return childComponent;
-    }
-  }
-
   render() {
-    return <div>{this.renderComponent()}</div>;
+    return this.props.children;
   }
 }
 
