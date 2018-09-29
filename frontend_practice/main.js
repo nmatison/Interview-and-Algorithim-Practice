33f493941cf0e2a5b9e47c43d13f77dc54035b8f
Nick <nmatison@gmail.com>
2018-09-29 10:12:27 -0700

    add onclick even handlers for each input

--- frontend_practice/main.js
+++ frontend_practice/main.js
@@ -1,12 +1,29 @@
-document.addEventListener("load",() => {
-  let target = document.getElementById("target");
-  target.innerHTML += `<input type="submit" onClick=${changeColor("blue")} value="Blue" />`;
-  target.innerHTML += `<input type="submit" onClick=${changeColor("green")} value="Green" />`;
-  target.innerHTML += `<input type="submit" onClick=${changeColor("red")} value="Red" />`;
-});
 
-const changeColor = function(color) {
-  let target = document.getElementById("target");
-  target.setAttribute("style", "background-color", `${color}`)
-
+var changeColor = function(color) {
+  
 };
+
+let target = document.getElementById("target");
+target.innerHTML += `<input class="blue" type="submit" value="Blue" />`;
+target.innerHTML += `<input class="green" type="submit" value="Green" />`;
+target.innerHTML += `<input class="red" type="submit" value="Red" />`;
+
+let color;
+
+document.querySelector(".blue").addEventListener("click", function(event) {
+    event.preventDefault;
+    let target = document.getElementById("target");
+    target.setAttribute("style", `background-color:blue`)
+  });
+
+document.querySelector(".green").addEventListener("click", function(event) {
+    event.preventDefault;
+    let target = document.getElementById("target");
+    target.setAttribute("style", `background-color:green`)
+  });
+
+document.querySelector(".red").addEventListener("click", function(event) {
+    event.preventDefault;
+    let target = document.getElementById("target");
+    target.setAttribute("style", `background-color:red`)
+  });
