909a258e9c3fc589c4805e839f8ba3a1773c316b
Nick <nmatison@gmail.com>
2018-10-06 22:04:52 -0700

    Pass topological sort specs in graph practice using Khan's algorithim

--- algorithim and data type practice/graph/lib/topological_sort.rb
+++ algorithim and data type practice/graph/lib/topological_sort.rb
@@ -2,6 +2,26 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
-def topological_sort(vertices)
 
+#Khan's algorithim
+def topological_sort(vertices)
+  in_edges = {}
+  zero_degree = []
+  list = []
+  vertices.each do |node| 
+    in_edges[node.value] = node.in_edges.length
+    zero_degree << node if in_edges[node.value] == 0
+  end
+  until zero_degree.empty?
+    current_node = zero_degree.shift
+    temp_nodes = []
+    current_node.out_edges.each do |edge|
+      to_node = edge.to_vertex
+      in_edges[to_node.value] -= 1
+      zero_degree << to_node if in_edges[to_node.value] == 0
+    end
+    list << current_node
+  end
+  list.map{|node| node.value}
+  return list.length == vertices.length ? list : []
 end
