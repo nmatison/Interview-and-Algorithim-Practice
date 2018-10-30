89694c2f59755c519aeab46b6087c5050366d4e8
Nick <nmatison@gmail.com>
2018-10-29 18:05:08 -0700

    Start working on bst

--- algorithim and data type practice/binarysearchtree/lib/binary_search_tree.rb
+++ algorithim and data type practice/binarysearchtree/lib/binary_search_tree.rb
@@ -2,13 +2,28 @@
 # to methods as you see fit, or to create helper methods.
 
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if @root.nil?
+      @root = BSTNode.new(value)
+      return @root 
+    end
+
+    return insert_helper(root, value)
   end
 
   def find(value, tree_node = @root)
+    return tree_node if tree_node.value == value 
+    if value <= tree_node.value
+      return tree_node.left.nil? ? nil : find(value, tree_node.left)
+    else
+      return tree_node.right.nil? ? nil : find(value, tree_node.right)
+    end
   end
 
   def delete(value)
@@ -16,9 +31,10 @@ class BinarySearchTree
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    return tree_node.right.nil? ? tree_node : maximum(tree_node.right)
   end
 
-  def depth(tree_node = @root)
+  def depth(tree_node = @root, count = 0)
   end 
 
   def is_balanced?(tree_node = @root)
@@ -30,5 +46,21 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
-
+  def insert_helper(node, val)
+    if val <= node.value
+      if node.left.nil?
+          node.left = BSTNode.new(val)
+          return node.left 
+      else 
+        return insert_helper(node.left, val)
+      end
+    else 
+      if node.right.nil? 
+        node.right = BSTNode.new(val)
+        return node.right 
+      else 
+        return insert_helper(node.right, val)
+      end
+    end
+  end
 end
