8383c410c376a787c2160372cf9408988fe40a9b
Nick <nmatison@gmail.com>
2018-11-02 10:05:55 -0700

    Add specs for quicksort

--- algorithim and data type practice/graph/lib/topological_sort.rb
+++ algorithim and data type practice/graph/lib/topological_sort.rb
@@ -5,23 +5,34 @@ require_relative 'graph'
 
 #Khan's algorithim
 def topological_sort(vertices)
-  in_edges = {}
-  zero_degree = []
+  #khan
   list = []
-  vertices.each do |node| 
-    in_edges[node.value] = node.in_edges.length
-    zero_degree << node if in_edges[node.value] == 0
-  end
-  until zero_degree.empty?
-    current_node = zero_degree.shift
-    temp_nodes = []
-    current_node.out_edges.each do |edge|
-      to_node = edge.to_vertex
-      in_edges[to_node.value] -= 1
-      zero_degree << to_node if in_edges[to_node.value] == 0
+  queue = []
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      queue.push(vertex)
     end
-    list << current_node
   end
-  list.map{|node| node.value}
-  return list.length == vertices.length ? list : []
+
+  until queue.empty?
+    u = queue.pop
+    neighbors = []
+
+    dup = u.out_edges.dup
+    dup.each do |edge|
+      neighbors.push(edge.to_vertex)
+      edge.destroy!
+    end
+    
+
+    neighbors.each do |vertex|
+      if vertex.in_edges.length < 1
+        queue.push(vertex)
+      end
+    end
+
+    list.push(u)
+  end
+
+  list.length == vertices.length ? list : []
 end
